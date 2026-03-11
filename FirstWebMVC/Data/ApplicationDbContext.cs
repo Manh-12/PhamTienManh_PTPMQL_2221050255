d9e81444d5603d81c6e7f231a2ed472843a06ea8
Manh-12 <ptienmanh191@gmail.com>
2026-03-11 15:05:38 +0700

    baithuchanhso5

---
+++ FirstWebMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using FirstWebMVC.Models.Entities;
+
+namespace FirstWebMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
